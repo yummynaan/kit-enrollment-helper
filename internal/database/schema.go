ec8248c38778b50b5851532b6cd88d316d966935
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-03-17 23:39:34 +0900

    スキーマモデルの記述

---
+++ internal/database/schema.go
@@ -0,0 +1,37 @@
+package database
+
+import "time"
+
+type department struct {
+	ID      int64  `db:"id"`
+	Faculty string `db:"faculty"`
+	Field   string `db:"field"`
+	Program string `db:"program"`
+}
+
+type user struct {
+	ID             int64     `db:"id"`
+	Name           string    `db:"name"`
+	Email          string    `db:"email"`
+	PasswordHash   string    `db:"password_hash"`
+	EnrollmentYear int       `db:"enrollment_year"`
+	DepartmentID   int64     `db:"department_id"`
+	CreatedAt      time.Time `db:"created_at"`
+	UpdatedAt      time.Time `db:"updated_at"`
+}
+
+type course struct {
+	ID          int64   `db:"id"`
+	TimetableID *int64  `db:"timetable_id"`
+	Class       *string `db:"class"`
+	Type        string  `db:"type"`
+	Credits     int     `db:"credits"`
+	Title       string  `db:"title"`
+	Year        int     `db:"year"`
+	Semester    string  `db:"semester"`
+	Day         string  `db:"day"`
+}
+
+func (c course) columns() []string {
+	return []string{"id", "timetable_id", "class", "type", "credits", "title", "year", "semester", "day"}
+}
