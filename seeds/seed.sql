8dcccab79ab977298ab509c1b75b1038d5b3a5df
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-03-27 02:24:03 +0900

    seedの作成

---
+++ seeds/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO `departments` (faculty, field, program) VALUES ('工芸科学部', '応用生物学域', '応用生物学課程');
+INSERT INTO `departments` (faculty, field, program) VALUES ('工芸科学部', '物質・材料科学域', '応用化学課程');
+INSERT INTO `departments` (faculty, field, program) VALUES ('工芸科学部', '設計工学域', '電子システム工学課程');
+INSERT INTO `departments` (faculty, field, program) VALUES ('工芸科学部', '設計工学域', '情報工学課程');
+INSERT INTO `departments` (faculty, field, program) VALUES ('工芸科学部', '設計工学域', '機械工学課程');
+INSERT INTO `departments` (faculty, field, program) VALUES ('工芸科学部', 'デザイン科学域', 'デザイン・建築学課程');
